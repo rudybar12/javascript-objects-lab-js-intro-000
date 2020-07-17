e9a663e0f052918e3442e748eec6799e58e9aece
Rodolfo Barrientos <rudybar12@yahoo.com>
2020-07-17 03:24:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function destructivelyDeleteFromObjectByKey(object, key, value){
   object[key] = value
   return object
 }
-function deleteFromObjectByKey(object, key, value){
+function deleteFromObjectByKey(object, key){
   
 }
